59b8c05b5c4e64c4992e3e6597e3687e912cf653
chandrac-01 <cchatterjee108@gmail.com>
2025-03-10 22:11:02 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -6,7 +6,7 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
-    addItem: (state, action) => {
+    addItem: (state, action) => {                 // Reducer to add a new item to the cart
         const { name, image, cost } = action.payload;
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
@@ -15,9 +15,11 @@ export const CartSlice = createSlice({
             state.items.push({ name, image, cost, quantity: 1 });
         }
     },
+    // Reducer to remove an item from the cart based on the plant name
     removeItem: (state, action) => {
         state.items = state.items.filter(item => item.name !== action.payload);
     },
+    // Reducer to update the quantity of an item in the cart
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
